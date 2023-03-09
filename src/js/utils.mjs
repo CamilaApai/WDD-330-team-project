1ddc310650d134241b6b6813bb19cf92b5b55e3c
CamilaApai <Camilaapai@hotmail.com>
2023-03-09 19:27:52 -0300

    Week 3 Team Activity

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -46,3 +46,28 @@ export function renderListWithTemplate(templateFn, parentElement, list, position
   }
   parentElement.insertAdjacentHTML(position, htmlStrings.join(""));
 }
+
+//REVIEW
+export async function renderWithTemplate(templateFn, parentElement, data, callback, position = "afterbegin") {
+  const htmlStrings = data.map(templateFn);
+  if (callback) {
+    callback(data);
+  }
+  parentElement.insertAdjacentHTML(position, htmlStrings.join(""));
+}
+
+export async function loadHeaderFooter() {
+  const header = document.getElementById("main-header");
+  const footer = document.getElementById("main-footer");
+  const headerTemplate = await loadTemplate("../partials/header.html");
+  const footerTemplate = await loadTemplate("../partials/footer.html");
+
+  renderWithTemplate(headerTemplate, header);
+  renderWithTemplate(footerTemplate, footer);
+}
+
+export async function loadTemplate(path) {
+  const fetchpath = await fetch(path);
+  const template = await fetchpath.text();
+  return template;
+}
