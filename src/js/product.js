a8285a37ce6a5456cf508c569a30e8547ef31dd0
Jarliks <alecmateski@gmail.com>
2023-03-12 17:07:46 -0700

    Add Item to cart fixed

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,14 @@
-import { setLocalStorage } from "./utils.mjs";
+import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
+var temp_cart = [];
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  temp_cart = [];
+  temp_cart = getLocalStorage("so-cart");
+  temp_cart.push(product);
+  setLocalStorage("so-cart", temp_cart);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
